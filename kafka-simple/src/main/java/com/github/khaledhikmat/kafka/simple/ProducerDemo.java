08ccfcd2638bc0aba13ade19a5f4892fbccc480f
Khaled Hikmat <khaled.hikmat@gmail.com>
2019-10-20 14:32:59 -0500

    Refactored the code into modules

---
+++ kafka-simple/src/main/java/com/github/khaledhikmat/kafka/simple/ProducerDemo.java
@@ -0,0 +1,29 @@
+package com.github.khaledhikmat.kafka.simple;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Producer props
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        // Send data asynch and flush
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "Hello World");
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
